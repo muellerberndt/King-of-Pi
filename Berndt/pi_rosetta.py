4108ef023c14c8c51c7ffee2a8b5fa86cc439f36
Bernhard Mueller <bernhardmueller@multipoarneuron.lan>
2022-07-21 08:33:54 +0700

    Finalizen benchmark and example

---
+++ Berndt/pi_rosetta.py
@@ -0,0 +1,25 @@
+# Algo: https://rosettacode.org/wiki/Pi#Python
+
+pi_digits = str()
+
+q, r, t, k, n, l, c = 1, 0, 1, 1, 3, 3, 0
+
+while c <= 10000:
+  if 4*q+r-t < n*t:
+      pi_digits += (str(n))
+      nr = 10*(r-n*t)
+      n  = ((10*(3*q+r))//t)-10*n
+      q  *= 10
+      r  = nr
+      c += 1
+  else:
+      nr = (2*q+r)*l
+      nn = (q*(7*k)+2+(r*l))//(t*l)
+      q  *= k
+      t  *= l
+      l  += 2
+      k += 1
+      n  = nn
+      r  = nr
+ 
+print("3.{}".format(pi_digits[1:]))
